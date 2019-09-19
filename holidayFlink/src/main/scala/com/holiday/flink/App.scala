855eebeeb38d0a4e485d1059b965e009eb2a8fd6
holiday <waiyou148@126.com>
2019-09-19 23:35:16 +0800

    batch and stream demo

--- holidayFlink/src/main/scala/com/holiday/flink/App.scala
+++ holidayFlink/src/main/scala/com/holiday/flink/App.scala
@@ -8,4 +8,5 @@ object AppScala   {
   def main(args: Array[String]): Unit = {
     println( "Hello World!" )
   }
+
 }
---
+++ holidayFlink/src/main/scala/com/holiday/flink/train/StreamingWCScalaApp.scala
@@ -0,0 +1,19 @@
+package com.holiday.flink.train
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.api.scala._
+object StreamingWCScalaApp {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val text = env.socketTextStream("192.168.226.128", 9993)
+    val words = text.flatMap(_.split(","))
+      .map((_, 1))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+    words.print()
+    env.execute()
+  }
+}
